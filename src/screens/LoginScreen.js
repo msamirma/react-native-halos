d1a10f1983a930443501da3227c3f16048bdb607
Andrew Fenner <andrewfenner@numbershapes.com>
2018-08-08 16:14:27 -0400

    login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,7 +4,7 @@ import ReactNative from "react-native";
 //import {App} from './App';
 import Dimensions from "Dimensions";
 
-import { RkButton } from "react-native-ui-kitten";
+import { RkButton, RkTextInput, RkTheme } from "react-native-ui-kitten";
 
 const {
   View,
@@ -16,7 +16,7 @@ const {
   Image
 } = ReactNative;
 
-let halosBackground = require("../assets/halos-mobile-background.jpg");
+let halosBackground = require("../assets/halos-mobile-background.png");
 let halosLogo = require("../assets/halos-logo.png");
 
 class HomeScreen extends Component {
@@ -30,16 +30,39 @@ class HomeScreen extends Component {
 
     return (
       <ImageBackground source={halosBackground} style={styles.backGroundImage}>
+        <View style={styles.logoContainer}>
+          <Image style={styles.headerLogo} source={halosLogo} />
+        </View>
         <View style={styles.mainContainer}>
-          <View>
-            <RkButton
-              style={styles.button}
-              onPress={() => {
-                navigate("App");
-              }}
-            >
-              Login
-            </RkButton>
+          <View style={styles.loginWrapper}>
+            <RkTextInput
+              style={styles.textInputs}
+              rkType="loginField"
+              placeholder="Username"
+            />
+            <RkTextInput
+              style={styles.textInputs}
+              rkType="loginField"
+              placeholder="Password"
+            />
+            <View style={styles.actionButtonsContainer}>
+              <RkButton
+                style={styles.actionButton}
+                onPress={() => {
+                  navigate("App");
+                }}
+              >
+                Login
+              </RkButton>
+              <RkButton
+                style={styles.actionButton}
+                onPress={() => {
+                  navigate("App");
+                }}
+              >
+                Sign Up
+              </RkButton>
+            </View>
           </View>
         </View>
       </ImageBackground>
@@ -51,32 +74,65 @@ let Window = Dimensions.get("window");
 let windowWidth = Window.width;
 let windowHeight = Window.height;
 
+let colored = false;
+
+let clear = "#ffffff00";
+let grey = colored ? "#6b6b6b" : clear;
+let blue = colored ? "#828cff" : clear;
+let red = colored ? "#c6356d" : clear;
+let yellow = colored ? "#fffb49" : clear;
+let orange = colored ? "#ff9b21" : clear;
+
+RkTheme.setType("RkTextInput", "loginField", {
+  placeholderTextColor: "white"
+});
+
 let styles = StyleSheet.create({
+  logoContainer: {
+    marginTop: 30,
+    height: 60,
+    width: "100%",
+    backgroundColor: blue
+  },
+  headerLogo: {
+    flex: 1,
+    alignSelf: "stretch",
+    width: undefined,
+    height: "100%",
+    backgroundColor: grey
+  },
   mainContainer: {
     flex: 1,
     marginTop: 20,
     flexDirection: "column",
-    alignItems: "center"
-  },
-  button: {
-    backgroundColor: "#c00ffe"
-  },
-  header: {
-    marginTop: 50,
-    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
     width: "100%",
+    backgroundColor: orange
+  },
+  textInputs: {
+    width: "80%",
+    backgroundColor: red
+  },
+  actionButtonsContainer: {
+    flex: 1,
     flexDirection: "row",
-    backgroundColor: "#c00ffe"
+    backgroundColor: blue,
+    justifyContent: "center"
   },
   backGroundImage: {
     flex: 1
   },
-  playbutton: {
-    marginTop: 50,
-    height: windowWidth / 8,
-    width: windowWidth / 3,
-    alignItems: "center"
-    //backgroundColor:'#2c3e50'
+  loginWrapper: {
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: yellow,
+    height: windowHeight / 3
+  },
+  actionButton: {
+    flex: 1,
+    backgroundColor: "#ffffff30",
+    borderRadius: 10
   }
 });
 
